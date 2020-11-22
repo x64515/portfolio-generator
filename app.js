184baf45e35629691290e51edbce988d2c89863f
Xavier <X64515@yahoo.com>
2020-11-22 07:14:50 -0600

    inquirer prompt

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?'
+//     }
+//   ])
+//   .then(answers => console.log(answers)
+//   var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
